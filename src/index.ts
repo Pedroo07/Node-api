4a79c7a369eaef6e0a9d53f2699f9ae8b1db63c0
Pedroo07 <pedrohenriquemoalive@gmail.com>
2026-02-25 11:51:51 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import {server } from "./server/server";
+import { Knex } from "./server/database/knex";
+import { server } from "./server/server";
+const StartServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log("ta funcionando");
+  });
+};
 
-server.listen(process.env.PORT, () => {
-  console.log("ta funcionando")
-}) 
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate.latest().then(() => {
+    StartServer()
+  }).catch(console.log)
+}else{
+  StartServer()
+}
